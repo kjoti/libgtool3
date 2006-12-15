7e36177884abbc84ac75051e3a56d9114648adbc
Koji OGOCHI <ogochi@jamstec.go.jp>
2006-12-15 01:58:44 +0000

    -xyz option added for range-slicing

--- ngtsumm.c
+++ ngtsumm.c
@@ -11,11 +11,17 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "gtool3.h"
 #include "seq.h"
 #include "fileiter.h"
+
+#ifndef min
+#  define min(a,b) ((a) < (b) ? (a) : (b))
+#endif
+
 
 static const char *usage_messages =
 	"Usage: ngtsumm [options] [files...]\n"
@@ -24,20 +30,27 @@ static const char *usage_messages =
 	"\n"
 	"Options:\n"
 	"    -h        print help message\n"
+	"    -l        print for each z-plane\n"
 	"    -t LIST   specify a list of data numbers\n"
-	"              LIST := S[,S[,S...]]\n"
-	"              S    := N1[:N2[:N3]] | :[N2[:N3]] | ::[N3]\n"
-	"              ex.)  :5        => 1 2 3 4 5\n"
-	"                    2:10:2    => 2 4 6 8 10\n"
-	"                    10:       => 10 11 ... LAST\n"
-	"                    -5:-1     => LAST-4 LAST-3 ... LAST\n"
-	"    -z        print for each z-plane\n";
+	"    -x RANGE  specify x-range\n"
+	"    -y RANGE  specify y-range\n"
+	"    -z RANGE  specify z-range\n"
+	"\n"
+	"    RANGE  := start[:[end]] | :[end]\n"
+	"    LIST   := RANGE[,RANGE]*\n";
 
+static int each_plane = 0;
+static int xrange[] = { 0, 0x7ffffff };
+static int yrange[] = { 0, 0x7ffffff };
+static int zrange[] = { 0, 0x7ffffff };
 static int slicing = 0;
+
 
 struct data_profile {
 	double vmin, vmax;
 	int miss_cnt, nan_cnt, inf_cnt;
+
+	double miss; /* used as input data */
 };
 
 
@@ -53,18 +66,15 @@ init_profile(struct data_profile *prof)
 
 
 void
-get_dataprof_float(const GT3_Varbuf *var, struct data_profile *prof)
+get_dataprof_float(const void *ptr, int len, struct data_profile *prof)
 {
-	const float *data = (const float *)var->data;
+	const float *data = (const float *)ptr;
 	int mcnt = 0, nan_cnt = 0, minf = 0, pinf = 0;
 	float miss;
 	float minc, maxc;
-	int i, len;
+	int i;
 
-	len = var->dimlen[0] * var->dimlen[1];
-	assert(var->bufsize >= 4 * len);
-
-	miss = (float)var->miss;
+	miss = (float)prof->miss;
 	minc = (float)prof->vmin;
 	maxc = (float)prof->vmax;
 
@@ -102,20 +112,18 @@ get_dataprof_float(const GT3_Varbuf *var, struct data_profile *prof)
 
 
 void
-get_dataprof_double(const GT3_Varbuf *var, struct data_profile *prof)
+get_dataprof_double(const void *ptr, int len, struct data_profile *prof)
 {
-	const double *data = (const double *)var->data;
+	const double *data = (const double *)ptr;
 	int mcnt = 0, nan_cnt = 0, minf = 0, pinf = 0;
 	double miss;
 	double minc, maxc;
-	int i, len;
+	int i;
 
-	len = var->dimlen[0] * var->dimlen[1];
-	assert(var->bufsize >= 8 * len);
-
-	miss = var->miss;
+	miss = prof->miss;
 	minc = prof->vmin;
 	maxc = prof->vmax;
+
 	for (i = 0; i < len; i++) {
 		if (data[i] == miss) {
 			mcnt++;
@@ -149,6 +157,44 @@ get_dataprof_double(const GT3_Varbuf *var, struct data_profile *prof)
 }
 
 
+int
+pack_slice_float(void *ptr, const GT3_Varbuf *var)
+{
+	const float *data = (const float *)var->data;
+	float *output = (float *)ptr;
+	int cnt = 0;
+	int i, j;
+	int xmax, ymax;
+
+	xmax = min(xrange[1], var->dimlen[0]);
+	ymax = min(yrange[1], var->dimlen[1]);
+	for (j = yrange[0]; j < ymax; j++)
+		for (i = xrange[0]; i < xmax; i++)
+			output[cnt++] = data[j * var->dimlen[0] + i];
+
+	return cnt;
+}
+
+
+int
+pack_slice_double(void *ptr, const GT3_Varbuf *var)
+{
+	const double *data = (const double *)var->data;
+	double *output = (double *)ptr;
+	int cnt = 0;
+	int i, j;
+	int xmax, ymax;
+
+	xmax = min(xrange[1], var->dimlen[0]);
+	ymax = min(yrange[1], var->dimlen[1]);
+	for (j = yrange[0]; j < ymax; j++)
+		for (i = xrange[0]; i < xmax; i++)
+			output[cnt++] = data[j * var->dimlen[0] + i];
+
+	return cnt;
+}
+
+
 void
 print_profile(FILE *output, struct data_profile *prof)
 {
@@ -162,11 +208,14 @@ print_profile(FILE *output, struct data_profile *prof)
 int
 print_summary(FILE *output, GT3_Varbuf *var)
 {
-	int z;
+	int z, zmax;
 	struct data_profile prof;
 	char item[17];
-	void (*get_dataprof)(const GT3_Varbuf *, struct data_profile *);
-
+	void (*get_dataprof)(const void *, int, struct data_profile *);
+	int (*pack_slice)(void *, const GT3_Varbuf *);
+	int rval, len, elem_size;
+	void *data;
+	int len2;
 
 	if (GT3_readVarZ(var, 0) < 0)
 		return -1;
@@ -174,29 +223,58 @@ print_summary(FILE *output, GT3_Varbuf *var)
 	GT3_getVarAttrStr(item, sizeof item, var, "ITEM");
 	fprintf(output, " %-12s", item);
 
-	get_dataprof = (var->type == GT3_TYPE_FLOAT)
-		? get_dataprof_float
-		: get_dataprof_double;
+	if (var->type == GT3_TYPE_FLOAT) {
+		get_dataprof = get_dataprof_float;
+		pack_slice   = pack_slice_float;
+		elem_size    = 4;
+	} else {
+		get_dataprof = get_dataprof_double;
+		pack_slice   = pack_slice_double;
+		elem_size    = 8;
+	}
+
+	if (slicing) {
+		int xlen, ylen;
+
+		xlen = min(var->dimlen[0], xrange[1] - xrange[0]);
+		ylen = min(var->dimlen[1], yrange[1] - yrange[0]);
+		if (xlen <= 0 || ylen <= 0
+			|| (data = malloc(xlen * ylen * elem_size)) == NULL)
+			return -1;
+	} else {
+		data = var->data;
+		len  = var->dimlen[0] * var->dimlen[1];
+	}
 
 	init_profile(&prof);
-	for (z = 0; z < var->dimlen[2]; z++) {
-		if (GT3_readVarZ(var, z) < 0)
-			return -1;
+	prof.miss = var->miss;
 
-		get_dataprof(var, &prof);
-		if (slicing) {
-			fprintf(output, z == 0 ? " %4d" : " %22d", z + 1);
+	rval = -1;
+	zmax = min(zrange[1], var->dimlen[2]);
+	for (z = zrange[0]; z < zmax; z++) {
+		if ((rval = GT3_readVarZ(var, z)) < 0)
+			break;
+
+		if (slicing)
+			len = pack_slice(data, var);
+
+		get_dataprof(data, len, &prof);
+		if (each_plane) {
+			fprintf(output, z == zrange[0] ? " %4d" : " %22d", z + 1);
 			print_profile(output, &prof);
 			printf("\n");
 			init_profile(&prof);
 		}
 	}
 
-	if (!slicing) {
+	if (slicing)
+		free(data);
+
+	if (rval == 0 && !each_plane) {
 		print_profile(output, &prof);
 		printf("\n");
 	}
-	return 0;
+	return rval;
 }
 
 
@@ -245,19 +323,77 @@ summ_file(const char *path, struct sequence *seq)
 }
 
 
+static int
+get_ints(int vals[], int numval, const char *str, char delim)
+{
+	const char *tail = str + strlen(str);
+	int cnt, num;
+	char *endptr;
+
+	cnt = 0;
+	while (str < tail && cnt < numval) {
+		num = (int)strtol(str, &endptr, 0);
+
+		if (*endptr != delim && *endptr != '\0')
+			return -1;			/* invalid char  */
+
+		if (*str != delim)
+			vals[cnt] = num;
+		str = endptr + 1;
+		cnt++;
+	}
+	return cnt;
+}
+
+
+static void
+set_range(int range[], const char *str)
+{
+	get_ints(range, 2, str, ':');
+
+	/*
+	 *  XXX
+	 *  transform
+	 *   FROM  1-offset and closed bound    [X,Y] => do i = X, Y.
+	 *   TO    0-offset and semi-open bound [X,Y) => for (i = X; i < Y; i++).
+	 */
+	range[0]--;
+	if (range[0] < 0)
+		range[0] = 0;
+
+	if (!strchr(str, ':'))
+		range[1] = range[0] + 1;
+}
+
+
 int
 main(int argc, char **argv)
 {
 	int ch;
 	struct sequence *seq = NULL;
 
-	while ((ch = getopt(argc, argv, "t:z")) != EOF)
+	while ((ch = getopt(argc, argv, "lt:x:y:z:")) != EOF)
 		switch (ch) {
+		case 'l':
+			each_plane = 1;
+			break;
+
 		case 't':
 			seq = initSeq(optarg, 1, 0x7fffffff);
 			break;
-		case 'z':
+
+		case 'x':
 			slicing = 1;
+			set_range(xrange, optarg);
+			break;
+
+		case 'y':
+			slicing = 1;
+			set_range(yrange, optarg);
+			break;
+
+		case 'z':
+			set_range(zrange, optarg);
 			break;
 
 		case 'h':
@@ -272,7 +408,7 @@ main(int argc, char **argv)
 
 	for (; argc > 0 && argv; --argc, ++argv) {
 		printf("# Filename: %s\n", *argv);
-		if (slicing)
+		if (each_plane)
 			printf("%-5s %12s %4s %14s %14s %10s %5s %5s\n",
 				   "#", "ITEM", "Z", "MIN", "MAX", "MISS", "NaN", "Inf");
 		else
