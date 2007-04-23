87e422d9f7e10f67e1ead518f4b79ffe3778b5c4
Koji OGOCHI <ogochi@jamstec.go.jp>
2007-04-23 01:34:52 +0000

    slicing functionarity added.

--- ngtcat.c
+++ ngtcat.c
@@ -5,14 +5,29 @@
  */
 #include "internal.h"
 
+#include <errno.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+
 #include "gtool3.h"
 #include "seq.h"
 #include "fileiter.h"
+
+#define PROGNAME "ngtcat"
+
+#define FH_SIZE 4
+#define URC_PARAMS_SIZE (4 + 4 + 8 + 6 * FH_SIZE)
+#define clip(v, l, h) ((v) < (l) ? (l) : ((v) > (h) ? (h) : v))
+#ifndef min
+#  define min(a, b) ((a) < (b) ? (a) : (b))
+#endif
+#ifndef max
+#  define max(a, b) ((a) > (b) ? (a) : (b))
+#endif
 
 
 static const char *usage_messages =
@@ -29,6 +44,43 @@ static const char *usage_messages =
 	"    RANGE  := start[:[end]] | :[end]\n";
 
 
+static int slicing = 0;
+static char *zslice_str = NULL;
+static int global_xrange[] = { 0, 0x7fffffff };
+static int global_yrange[] = { 0, 0x7fffffff };
+
+
+static void
+myperror(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	if (errno != 0) {
+		fprintf(stderr, "%s:", PROGNAME);
+		if (fmt) {
+			vfprintf(stderr, fmt, ap);
+			fprintf(stderr, ":");
+		}
+		fprintf(stderr, " %s\n", strerror(errno));
+	}
+	va_end(ap);
+}
+
+
+static int
+write_header(const GT3_HEADER *head, FILE *fp)
+{
+	unsigned char siz[] = { 0, 0, 4, 0 };
+	unsigned char buf[GT3_HEADER_SIZE + 8];
+
+	memcpy(buf, siz, 4);
+	memcpy(buf + 4, head->h, GT3_HEADER_SIZE);
+	memcpy(buf + 4 + GT3_HEADER_SIZE, siz, 4);
+	return (fwrite(buf, 1, sizeof buf, fp) != sizeof buf) ? -1 : 0;
+}
+
+
 static int
 fcopy(FILE *dest, FILE *src, size_t size)
 {
@@ -43,6 +95,299 @@ fcopy(FILE *dest, FILE *src, size_t size)
 		size -= nread;
 	}
 	return (size == 0) ? 0 : -1;
+}
+
+
+static int
+test_seq(int *first, int *order, struct sequence *seq, int low, int up)
+{
+	struct sequence temp = *seq;
+	int cnt = 0;
+	int prev = -1;
+
+	*order = 0;
+	while (nextSeq(&temp))
+		if (temp.curr >= low && temp.curr <= up) {
+			*order |= (cnt > 0 && prev != temp.curr - 1);
+			if (cnt == 0)
+				*first = temp.curr;
+
+			cnt++;
+			prev = temp.curr;
+		}
+	return cnt;
+}
+
+
+static int
+slicecopy2(FILE *dest, GT3_File *fp, size_t esize,
+		   int xrange[], int yrange[],
+		   int zstr, int zstep, int znum)
+{
+	int y, z;
+	size_t siz = 0, ssize;
+	off_t off, zoff, off0 = 0;
+
+
+	if (fp->fmt == GT3_FMT_URC || fp->fmt == GT3_FMT_URC1) {
+		siz = esize * (xrange[1] - xrange[0]) * (yrange[1] - yrange[0]);
+		if (IS_LITTLE_ENDIAN)
+			reverse_words(&siz, 1);
+		off0 = URC_PARAMS_SIZE + 4;
+	}
+
+	ssize = esize * (xrange[1] - xrange[0]);
+	for (z = zstr; znum > 0; znum--, z += zstep) {
+		/*
+		 *  XXX Out-of-range is not treated as an error.
+		 */
+		if (z < 0 || z >= fp->dimlen[2])
+			continue;
+
+		if (GT3_skipZ(fp, z) < 0)
+			return -1;
+
+		zoff = ftello(fp->fp);
+
+		if (fp->fmt == GT3_FMT_URC || fp->fmt == GT3_FMT_URC1) {
+			/* write urc parameters */
+			/* & fortran header */
+			if (fcopy(dest, fp->fp, URC_PARAMS_SIZE) < 0
+				|| fwrite(&siz, 1, 4, dest) != 4)
+				return -1;
+		}
+
+		for (y = yrange[0]; y < yrange[1]; y++) {
+			off = zoff + off0 + esize * (fp->dimlen[0] * y + xrange[0]);
+
+			if (fseeko(fp->fp, off, SEEK_SET) < 0
+				|| fcopy(dest, fp->fp, ssize) < 0)
+				return -1;
+		}
+
+		if (fp->fmt == GT3_FMT_URC || fp->fmt == GT3_FMT_URC1) {
+			/* fortran trailer */
+			if (fwrite(&siz, 1, 4, dest) != 4)
+				return -1;
+		}
+	}
+	return 0;
+}
+
+
+static int
+slicecopy(FILE *dest, GT3_File *fp)
+{
+	size_t siz, esize, ssize;
+	GT3_HEADER head;
+	struct sequence *zseq;
+	int xrange[2];
+	int yrange[2];
+	int xstr0, ystr0, zstr0, zend0;
+	int zstr, zorder;
+	int xynum, znum, nz;
+	int all_flag;
+
+
+	if (GT3_readHeader(&head, fp) < 0
+		|| GT3_decodeHeaderInt(&xstr0, &head, "ASTR1") < 0
+		|| GT3_decodeHeaderInt(&ystr0, &head, "ASTR2") < 0
+		|| GT3_decodeHeaderInt(&zstr0, &head, "ASTR3") < 0
+		|| GT3_decodeHeaderInt(&zend0, &head, "AEND3") < 0)
+		return -1;
+
+	xrange[0] = max(0, global_xrange[0] - xstr0 + 1);
+	yrange[0] = max(0, global_yrange[0] - ystr0 + 1);
+	xrange[1] = min(global_xrange[1], fp->dimlen[0]);
+	yrange[1] = min(global_yrange[1], fp->dimlen[1]);
+
+	zseq = initSeq(zslice_str ? zslice_str : ":", zstr0, zend0);
+	znum = test_seq(&zstr, &zorder, zseq, zstr0, zend0);
+
+	if (znum <= 0
+		|| xrange[1] - xrange[0] <= 0
+		|| yrange[1] - yrange[0] <= 0 ) {
+		fprintf(stderr, "%s: No data in specified domain.\n", PROGNAME);
+		return -1; /* NO data to copy */
+	}
+	switch (fp->fmt) {
+	case GT3_FMT_UR4:
+		esize = 4;
+		break;
+	case GT3_FMT_URC:
+	case GT3_FMT_URC1:
+		esize = 2;
+		break;
+	case GT3_FMT_UR8:
+		esize = 8;
+		break;
+	default:
+		/* NOTREACHED */
+		esize = 4;
+		break;
+	}
+
+	xynum = (xrange[1] - xrange[0]) * (yrange[1] - yrange[0]);
+	all_flag = xrange[1] - xrange[0] == fp->dimlen[0]
+		    && yrange[1] - yrange[0] == fp->dimlen[1];
+
+	/*
+	 *  modify header field.
+	 */
+	GT3_setHeaderInt(&head, "ASTR1", xrange[0] + xstr0);
+	GT3_setHeaderInt(&head, "AEND1", xrange[1] + xstr0 - 1);
+	GT3_setHeaderInt(&head, "ASTR2", yrange[0] + ystr0);
+	GT3_setHeaderInt(&head, "AEND2", yrange[1] + ystr0 - 1);
+	GT3_setHeaderInt(&head, "SIZE",  xynum * znum);
+	if (zorder) {
+		/*
+		 *  In this case, the z-axis name cannot remain the same.
+		 */
+		zstr = 1;
+		GT3_setHeaderString(&head, "AITM3", "NUMBER1000");
+	}
+	GT3_setHeaderInt(&head, "ASTR3", zstr);
+	GT3_setHeaderInt(&head, "AEND3", zstr + znum - 1);
+
+	/* write header */
+	if (write_header(&head, dest) < 0)
+		return -1;
+
+	/*
+	 *  for UR4 or UR8, Fotrran header
+	 */
+	if (fp->fmt == GT3_FMT_UR4 || fp->fmt == GT3_FMT_UR8) {
+		siz = esize * xynum * znum;
+		if (IS_LITTLE_ENDIAN)
+			reverse_words(&siz, 1);
+
+		if (fwrite(&siz, 1, 4, dest) != 4)
+			return -1;
+	}
+
+	/*
+	 *  iterate z-slice.
+	 */
+	reinitSeq(zseq, zseq->first, zseq->last);
+	while (nextToken(zseq)) {
+		nz = (zseq->step == 0)
+			? 1
+			: (zseq->tail - zseq->head + zseq->step) / zseq->step;
+
+		if (all_flag && zseq->step == 1) {
+			/*
+			 *  Simple slicing:
+			 *  In this case, data in this slice are contiguous.
+			 */
+			int zpos;
+
+			/*
+			 *  z-index check.
+			 */
+			zpos = clip(zseq->head, zstr0, zend0) - zstr0;			
+			nz   = clip(zseq->tail, zstr0, zend0) - (zpos + zstr0) + 1;
+			if (nz <= 0)
+				continue;
+
+			ssize = esize * xynum * nz;
+
+			if (fp->fmt == GT3_FMT_URC || fp->fmt == GT3_FMT_URC1)
+				ssize += (URC_PARAMS_SIZE + 2 * FH_SIZE) * nz;
+
+			if (GT3_skipZ(fp, zpos) < 0 || fcopy(dest, fp->fp, ssize) < 0)
+				return -1;
+		} else if (all_flag) {
+			int z;
+
+			ssize = esize * xynum;
+			if (fp->fmt == GT3_FMT_URC || fp->fmt == GT3_FMT_URC1)
+				ssize += URC_PARAMS_SIZE + 2 * FH_SIZE;
+
+			for (z = zseq->head - zstr0; nz-- > 0; z += zseq->step) {
+				if (z < 0 || z >= fp->dimlen[2])
+					continue;
+
+				if (GT3_skipZ(fp, z) < 0 || fcopy(dest, fp->fp, ssize) < 0)
+					return -1;
+			}
+		} else {
+			/* more detailed slicing */
+			if (slicecopy2(dest, fp, esize,
+						   xrange, yrange,
+						   zseq->head - zstr0, zseq->step, nz) < 0)
+				return -1;
+		}
+	}
+
+	/*
+	 *  for UR4 or UR8, Fotrran trailer
+	 */
+	if (fp->fmt == GT3_FMT_UR4 || fp->fmt == GT3_FMT_UR8)
+		if (fwrite(&siz, 1, 4, dest) != 4)
+			return -1;
+
+	freeSeq(zseq);
+	free(zseq);
+	return 0;
+}
+
+
+static int
+mcopy(FILE *dest, GT3_File *fp)
+{
+	int rval;
+
+	if (slicing) {
+		if ((rval = slicecopy(dest, fp)) < 0)
+			GT3_printErrorMessages(stderr);
+	} else
+		rval = fcopy(dest, fp->fp, fp->chsize);
+
+	return rval;
+}
+
+
+static int
+get_ints(int vals[], int numval, const char *str, char delim)
+{
+	const char *tail = str + strlen(str);
+	int cnt, num;
+	char *endptr;
+
+	cnt = 0;
+	while (str < tail && cnt < numval) {
+		num = (int)strtol(str, &endptr, 0);
+
+		if (*endptr != delim && *endptr != '\0')
+			return -1;			/* invalid char  */
+
+		if (*str != delim)
+			vals[cnt] = num;
+		str = endptr + 1;
+		cnt++;
+	}
+	return cnt;
+}
+
+
+static int
+set_range(int range[], const char *str)
+{
+	if (get_ints(range, 2, str, ':') < 0)
+		return -1;
+
+	/*
+	 *  XXX
+	 *  transform
+	 *   FROM  1-offset and closed bound    [X,Y] => do i = X, Y.
+	 *   TO    0-offset and semi-open bound [X,Y) => for (i = X; i < Y; i++).
+	 */
+	range[0]--;
+	if (range[0] < 0)
+		range[0] = 0;
+	if (!strchr(str, ':'))
+		range[1] = range[0] + 1;
+	return 0;
 }
 
 
@@ -74,8 +419,8 @@ gtcat_cyclic(int num, char *path[], struct sequence *seq)
 				: GT3_seek(fp, seq->curr, SEEK_END);
 
 			if (stat == 0) {
-				if (fcopy(stdout, fp->fp, fp->chsize) < 0) {
-					perror(path[i]);
+				if (mcopy(stdout, fp) < 0) {
+					myperror(path[i]);
 					errflag = 1;
 				}
 			} else if (stat != GT3_ERR_INDEX)
@@ -106,13 +451,18 @@ gtcat(const char *path, struct sequence *seq)
 		if (stat == ITER_OUTRANGE)
 			continue;
 
-		if (stat == ITER_ERROR || stat == ITER_ERRORCHUNK) {
+		if (stat == ITER_ERROR) {
+			fprintf(stderr, "%s: invalid specifier: %s", PROGNAME, seq->it);
+			break;
+		}
+
+		if (stat == ITER_ERRORCHUNK) {
 			rval = -1;
 			break;
 		}
 
-		if (fcopy(stdout, fp->fp, fp->chsize) < 0) {
-			perror(path);
+		if (mcopy(stdout, fp) < 0) {
+			myperror(path);
 			rval = -1;
 			break;
 		}
@@ -130,7 +480,7 @@ main(int argc, char **argv)
 	int cyclic = 0;
 	int ch, rval = 0;
 
-	while ((ch = getopt(argc, argv, "cht:")) != -1)
+	while ((ch = getopt(argc, argv, "cht:x:y:z:")) != -1)
 		switch (ch) {
 		case 'c':
 			cyclic = 1;
@@ -138,6 +488,29 @@ main(int argc, char **argv)
 
 		case 't':
 			seq = initSeq(optarg, 1, 0x7fffffff);
+			break;
+
+		case 'x':
+			if (set_range(global_xrange, optarg) < 0) {
+				fprintf(stderr, "%s: invalid argument for -x option: %s\n",
+						PROGNAME, optarg);
+				exit(1);
+			}
+			slicing = 1;
+			break;
+
+		case 'y':
+			if (set_range(global_yrange, optarg) < 0) {
+				fprintf(stderr, "%s: invalid argument for -y option: %s\n",
+						PROGNAME, optarg);
+				exit(1);
+			}
+			slicing = 1;
+			break;
+
+		case 'z':
+			zslice_str = strdup(optarg);
+			slicing = 1;
 			break;
 
 		case 'h':
@@ -152,7 +525,7 @@ main(int argc, char **argv)
 
 	argc -= optind;
 	argv += optind;
-	GT3_setProgname("ngtcat");
+	GT3_setProgname(PROGNAME);
 
 	if (cyclic) {
 		if (gtcat_cyclic(argc, argv, seq) < 0)
