69e01817185cb009652d14889ab06e63dbec94ab
Koji OGOCHI <ogochi@jamstec.go.jp>
2009-02-23 11:07:55 +0000

    add funcs in write-ury.c

--- write-fmt.h
+++ write-fmt.h
@@ -1,3 +1,6 @@
+#ifndef GT3_WRITE_FMT__H
+#define GT3_WRITE_FMT__H
+
 #include <sys/types.h>
 #include <stdio.h>
 
@@ -20,7 +23,7 @@ int write_mrx_via_float(const void *ptr,
 						FILE *fp);
 
 /* write-mask.c */
-unsigned masked_count(const void *ptr, size_t size, 
+unsigned masked_count(const void *ptr, size_t size,
 					  size_t nelems, double miss);
 int write_mask(const void *ptr2,
 			   size_t size,
@@ -38,3 +41,22 @@ int write_mr8_via_double(const double *data, size_t nelems,
 
 int write_mr8_via_float(const float *data, size_t nelems,
 						double miss, FILE *fp);
+
+/* write-ury.c */
+int write_ury_via_double(const void *ptr,
+						 size_t zelem, size_t nz,
+						 int nbits, double miss, FILE *fp);
+int write_ury_via_float(const void *ptr,
+						size_t zelem, size_t nz,
+						int nbits, double miss, FILE *fp);
+
+int write_mry_via_double(const void *ptr,
+						 size_t zelems, size_t nz,
+						 int nbits, double miss,
+						 FILE *fp);
+int write_mry_via_float(const void *ptr,
+						size_t zelems, size_t nz,
+						int nbits, double miss,
+						FILE *fp);
+
+#endif
